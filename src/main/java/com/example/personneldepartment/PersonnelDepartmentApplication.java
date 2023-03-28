e48e80af5e29c4637a648903da4809773b043559
Katya <ksemeukina@gmail.com>
2023-03-28 14:32:59 +0200

    Sixth Commit

--- src/main/java/com/example/personneldepartment/PersonnelDepartmentApplication.java
+++ src/main/java/com/example/personneldepartment/PersonnelDepartmentApplication.java
@@ -3,6 +3,10 @@ package com.example.personneldepartment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class PersonnelDepartmentApplication is a start program point
+ */
+
 @SpringBootApplication
 public class PersonnelDepartmentApplication {
 
